02743b1b35198eb0700dc3cf90527141e96280f6
Arbiter_Aryad <notanarbiter@gmail.com>
2022-08-13 02:48:26 +0530

    using real data during handshake

--- chat_system/middlewares/handshake.js
+++ chat_system/middlewares/handshake.js
@@ -9,27 +9,27 @@ const chatMiddleware = async (chat, ...args) => {
   console.log("Auth: ", socket.handshake.auth);
 
   // real values
-  // const { id = "" } = socket.handshake.auth;
-  // if (!id) {
-  //   console.log("No id specified");
-  //   socket.emit("error", "Authentication failed");
-  //   return;
-  // }
-  // const user = getUser(id); // need to complete this...depends on Subhra's endpoints
-  // if(typeof user === "string"){
-  //   console.log("No id specified");
-  //   socket.emit("error", user); // the fn returns string if user is not found
-  //   return;
-  // }
-  // socket.groups = user.groups; //which is an array of group rooms;
-  // socket.userID = user.room; // which is just the user's room;
+  const { id = "" } = socket.handshake.auth;
+  if (!id) {
+    console.log("No id specified");
+    socket.emit("error", "Authentication failed");
+    return;
+  }
+  const user = getUser(id); // need to complete this...depends on Subhra's endpoints
+  if(typeof user === "string"){
+    console.log("No id specified");
+    socket.emit("error", user); // the fn returns string if user is not found
+    return;
+  }
+  socket.groups = user.groups; //which is an array of group rooms;
+  socket.userID = user.room; // which is just the user's room;
 
   // dummy values
   // let userroom = "0000";
-  let rm = Math.round(Math.random() * 1000);
-  let userroom = `${rm}`;
-  socket.userID = userroom;
-  socket.groups = ["456"];
+  // let rm = Math.round(Math.random() * 1000);
+  // let userroom = `${rm}`;
+  // socket.userID = userroom;
+  // socket.groups = ["456"];
 
   next();
 };
