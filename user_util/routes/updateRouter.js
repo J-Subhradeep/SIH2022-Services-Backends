c09e43a0faa0266588a5dcf49184da93c3401bff
Subhradeep Pal <87785813+subhradee@users.noreply.github.com>
2022-08-13 19:09:30 +0530

    chat list done

--- user_util/routes/updateRouter.js
+++ user_util/routes/updateRouter.js
@@ -7,28 +7,22 @@ const dpMod = require("../middleware/modify/dpMod");
 const chatBgMod = require("../middleware/modify/chatBgMod");
 
 // http://localhost:8080/upload/upload_dp
-updateRouter
-.route("/upload_dp")
-.put(cloudinaryUpload, dpMod, cloudinaryRemove);
+updateRouter.route("/upload_dp").put(cloudinaryUpload, dpMod, cloudinaryRemove);
 
 // http://localhost:8080/upload/upload_cover
-updateRouter
-.route("/upload_cover")
-.put(
-  // uploader.single("util_file"),
-  cloudinaryUpload,
-  coverMod,
-  cloudinaryRemove
-  );
-  
-  // http://localhost:8080/upload/upload_chatbg
-updateRouter
-  .route("/upload_chatbg")
-  .put(
-    // uploader.single("util_file"),
-    cloudinaryUpload,
-    chatBgMod,
-    cloudinaryRemove
-  );
+updateRouter.route("/upload_cover").put(
+	// uploader.single("util_file"),
+	cloudinaryUpload,
+	coverMod,
+	cloudinaryRemove
+);
+
+// http://localhost:8080/upload/upload_chatbg
+updateRouter.route("/upload_chatbg").put(
+	// uploader.single("util_file"),
+	cloudinaryUpload,
+	chatBgMod,
+	cloudinaryRemove
+);
 
 module.exports = updateRouter;
