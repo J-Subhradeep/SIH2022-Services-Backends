c68d99e899e69df24f77b06ed3d33e5782dac845
Arbiter_Aryad <notanarbiter@gmail.com>
2022-08-19 20:38:10 +0530

    un share router updated

--- contentResponseSystem/routes/undoRouter.js
+++ contentResponseSystem/routes/undoRouter.js
@@ -1,12 +1,13 @@
 const unComment = require("../controllers/undo/unComment");
 const unLike = require("../controllers/undo/unLike");
+const unShare = require("../controllers/undo/unShare");
 const undoRouter = require("express").Router();
 
 undoRouter.patch("/like", unLike);
 
 undoRouter.patch("/comment", unComment);
 
+undoRouter.patch("/share", unShare);
+
 module.exports = undoRouter;
 
-// ,
-// 	"body":"suka blyat !?!"
