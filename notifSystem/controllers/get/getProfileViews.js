80ac72c11e21a9f1bf2d0f111aae5271f58ba378
Arbiter_Aryad <notanarbiter@gmail.com>
2022-08-19 23:20:05 +0530

    get endpoint set up

---
+++ notifSystem/controllers/get/getProfileViews.js
@@ -0,0 +1,16 @@
+const ProfileView = require("../../model/profileView");
+
+const getProfileViews = (req, res) => {
+  const { user = "" } = req.query;
+  if (!user) return res.status(400).send({ message: "Invalid credentials" });
+
+  try {
+    const views = await ProfileView.findById(user);
+    if(!views) return res.status(404).send({ message: "User not found" });
+    res.send({ views: views.profileViews})
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+};
+module.exports = getProfileViews;
--- notifSystem/routes/getRouter.js
+++ notifSystem/routes/getRouter.js
@@ -1,4 +1,5 @@
 const getAllNotifs = require("../controllers/get/getAllNotifs");
+const getProfileViews = require("../controllers/get/getProfileViews");
 const getTotalNotifs = require("../controllers/get/getTotalNotifs");
 const getUnseenNotifs = require("../controllers/get/getUnseenNotifs");
 
@@ -9,5 +10,7 @@ getRouter.get("/notifs", getAllNotifs);
 getRouter.get("/unseen", getUnseenNotifs);
 
 getRouter.get("/total", getTotalNotifs);
+
+getRouter.get("/views", getProfileViews);
 
 module.exports = getRouter;
