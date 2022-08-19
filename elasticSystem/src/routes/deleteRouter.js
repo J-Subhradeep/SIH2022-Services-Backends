70bd143eeaf111cf49f66ed781c7775973113d30
Arbiter_Aryad <notanarbiter@gmail.com>
2022-08-19 20:33:29 +0530

    fixing routers for deleting and uploading shares

--- elasticSystem/src/routes/deleteRouter.js
+++ elasticSystem/src/routes/deleteRouter.js
@@ -2,11 +2,12 @@ const deleteRouter = require("express").Router();
 const deleteController = require("../controllers/deleteController");
 const fetchMiddleware = require("../middlewares/fetchMiddleware");
 const delFromCloudinary = require("../middlewares/delFromCloudinary");
+const unShareController = require("../controllers/unShareController");
 
 deleteRouter
   .route("/")
   .delete(fetchMiddleware, delFromCloudinary, deleteController);
 
-deleteRouter.route("/delete_entry").delete(deleteController);
+deleteRouter.route("/delete_entry").delete(deleteController,unShareController);
 
 module.exports = deleteRouter;
