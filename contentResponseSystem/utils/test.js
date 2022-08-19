51acf78ad33421abf7f24627a2f18290efc4f1e7
Arbiter_Aryad <notanarbiter@gmail.com>
2022-08-19 20:36:13 +0530

    dummy input in test.js

--- contentResponseSystem/utils/test.js
+++ contentResponseSystem/utils/test.js
@@ -2,3 +2,36 @@
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // let ar2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // console.log(l.isEqual(ar2, arr));
+
+// const obj = {
+//   desc: "some text",
+//   owner_id: "bffa19f", // owner of the post
+//   is_shared: false,
+//   is_public: true,
+//   shared_from: "", //empty
+//   shared_from_owner: "", //empty
+//   like_count: 0,
+//   comment_count: 0,
+//   share_count: 0,
+//   file: [],
+//   access_url: [],
+//   id: "7fd94bb7-c246-4d18-81dd-a62fe4e79e03",
+//   time: "2022-08-17T19:22:27.000Z",
+// };
+// const objShared = {
+//   desc: "some text",
+//   owner_id: "bffa19f", // owner of the shared post
+//   is_shared: true,
+//   is_public: true,
+//   shared_from: "", //c_id
+//   shared_from_owner: "", // owner of the actual post
+//   like_count: 0,
+//   comment_count: 0,
+//   share_count: 0,
+//   id: "7fd94bb7-c246-4d18-81dd-a62fe4e79e03",
+//   time: "2022-08-17T19:22:27.000Z",
+// };
+
+// for (let i in obj) {
+//   console.log(obj[i] === objShared[i]);
+// }
