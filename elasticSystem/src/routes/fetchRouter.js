f38aa8f234aa863fba659ddfadc59cdd0a0f3a38
Arbiter_Aryad <notanarbiter@gmail.com>
2022-08-25 09:20:35 +0530

    fetch by id

--- elasticSystem/src/routes/fetchRouter.js
+++ elasticSystem/src/routes/fetchRouter.js
@@ -1,6 +1,9 @@
 const fetchRouter = require("express").Router();
+const fetchByIdController = require("../controllers/fetchByIdController");
 const fetchController = require("../controllers/fetchController")
 
 fetchRouter.route("/").get(fetchController)
+
+fetchRouter.route("/postid").get(fetchByIdController)
 
 module.exports = fetchRouter;
